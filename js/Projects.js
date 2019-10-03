16b89b37e7224ef9e6b5d2e6488326b777389bc5
jamesstevick <jstevick@umich.edu>
2019-10-03 09:31:34 -0700

    Basic structure outlined and popper library added

---
+++ js/Projects.js
@@ -0,0 +1,64 @@
+// This file contains data for my portfolio
+// The main object, "portfolio" contains an array of projects
+// Each project contains a pdf, image, and some metadata
+
+var portfolio = {
+	projects : [
+		{
+            title : "Magnetic Levitator", 
+            pdfFile : "reports/MagLev.pdf",
+            thumbnail : "images/MagLev.jpg",
+            description : "The MagLev system maintains the position of a para-magnetic ball by using position sensors to control the driver circuit of an electromagnet",
+            tags : ["hardware", "DIY", "course projects"],
+        },
+		{
+            title : "Magnetic Levitator", 
+            pdfFile : "reports/MagLev.pdf",
+            thumbnail : "images/MagLev.jpg",
+            description : "The MagLev system maintains the position of a para-magnetic ball by using position sensors to control the driver circuit of an electromagnet",
+            tags : ["hardware", "DIY", "course projects"]
+        },
+		{
+            title : "Magnetic Levitator", 
+            pdfFile : "reports/MagLev.pdf",
+            thumbnail : "images/MagLev.jpg",
+            description : "The MagLev system maintains the position of a para-magnetic ball by using position sensors to control the driver circuit of an electromagnet",
+            tags : ["hardware", "DIY", "course projects"]
+        },
+		{
+            title : "Magnetic Levitator", 
+            pdfFile : "reports/MagLev.pdf",
+            thumbnail : "images/MagLev.jpg",
+            description : "The MagLev system maintains the position of a para-magnetic ball by using position sensors to control the driver circuit of an electromagnet",
+            tags : ["hardware", "DIY", "course projects"]
+        },
+		{
+            title : "Magnetic Levitator", 
+            pdfFile : "reports/MagLev.pdf",
+            thumbnail : "images/MagLev.jpg",
+            description : "The MagLev system maintains the position of a para-magnetic ball by using position sensors to control the driver circuit of an electromagnet",
+            tags : ["hardware", "DIY", "course projects"]
+        },
+		{
+            title : "Magnetic Levitator", 
+            pdfFile : "reports/MagLev.pdf",
+            thumbnail : "images/MagLev.jpg",
+            description : "The MagLev system maintains the position of a para-magnetic ball by using position sensors to control the driver circuit of an electromagnet",
+            tags : ["hardware", "DIY", "course projects"]
+        },
+		{
+            title : "Magnetic Levitator", 
+            pdfFile : "reports/MagLev.pdf",
+            thumbnail : "images/MagLev.jpg",
+            description : "The MagLev system maintains the position of a para-magnetic ball by using position sensors to control the driver circuit of an electromagnet",
+            tags : ["hardware", "DIY", "course projects"]
+        },
+		{
+            title : "Magnetic Levitator", 
+            pdfFile : "reports/MagLev.pdf",
+            thumbnail : "images/MagLev.jpg",
+            description : "The MagLev system maintains the position of a para-magnetic ball by using position sensors to control the driver circuit of an electromagnet",
+            tags : ["hardware", "DIY", "course projects"]
+        }
+	]
+};
--- js/gallery.js
+++ js/gallery.js
@@ -1,7 +1,8 @@
 
-/*
- *		This file contains the javascript code for our gallery
- */
+/*****************************************************
+ *  This file contains the JavaScript for the content 
+ *  Written by James Stevick
+ *****************************************************/
 
 // variables for all of the templates so we only have to compile
 // them once on page load and can then use the same compiled 
@@ -15,11 +16,12 @@ var current_photo = current_album.photos[0];
 // a helper function that instantiates a template
 // and displays the results in the content div
 function showTemplate(template, data){
-	var html    = template(data);
+	var html = template(data);
 	$('#content').html(html);
 }
 
-// document read gets called when the whole document
+
+// document ready gets called when the whole document
 // is loaded, so we put most of the code that needs to run
 // in here
 $(document).ready(function(){
@@ -27,7 +29,14 @@ $(document).ready(function(){
 	//
 	// compile all of our templates ready for use
 	//
-	var source   = $("#albums-template").html();
+
+	var source = $("#projects-template").html();
+	projects_template = Handlebars.compile(source);
+
+	source = $("#project-template").html();
+	project_template = Handlebars.compile(source);
+
+	source   = $("#albums-template").html();
 	albums_template = Handlebars.compile(source);
 	
 	source   = $("#photos-template").html();
@@ -38,21 +47,21 @@ $(document).ready(function(){
 	
 	source   = $("#slideshow-template").html();
 	slideshow_template = Handlebars.compile(source);
-
+ 
 	// 
 	//  clicking on the albums tab shows the 
 	//  thumbnails of all the albums
 	//
-	$("#albums-tab").click(function () {
+	$("#example-tab").click(function () {
 
 		// displays the albums template
 		showTemplate(albums_template, gallery);
 
 		// make the albums tab the active one
 		// first make the currently active tab inactive
-		$(".nav-tabs .active").removeClass("active");
+		$(".nav-pills .active").removeClass("active");
 		// then make albums tab active
-		$("#albums-tab").addClass("active");
+		$("#example-tab").addClass("active");
 
 		// add a click callback to each album 
 		// thumbnail which displays the photos
@@ -102,16 +111,16 @@ $(document).ready(function(){
 	//  clicking on the photos tab shows all of the 
 	//  photos in the current album
 	//
-	$("#photos-tab").click(function () {
+	$("#papers-tab").click(function () {
 		
 		// displays the photos template
 		showTemplate(photos_template, current_album);
 
 		// make the photos tab the active one
 		// first make the currently active tab inactive
-		$(".nav-tabs .active").removeClass("active");
+		$(".nav-pills .active").removeClass("active");
 		// then make photos tab active
-		$("#photos-tab").addClass("active");
+		$("#papers-tab").addClass("active");
 
 		// add an on click al all the photo thumbnails
 		// which displays the photo in a modal popup
@@ -132,6 +141,48 @@ $(document).ready(function(){
 		});
 	});
 
+
+
+
+	// 
+	//  clicking on the photos tab shows all of the 
+	//  photos in the current album
+	//
+	$("#projects-tab").click(function () {
+		
+		// displays the photos template
+		showTemplate(projects_template, portfolio);
+
+		// make the photos tab the active one
+		// first make the currently active tab inactive
+		$(".nav-pills .active").removeClass("active");
+		// then make photos tab active
+		$("#projects-tab").addClass("active");
+
+		// add an on click al all the photo thumbnails
+		// which displays the photo in a modal popup
+		$(".project-thumbnail").click(function (){
+			// get the index (position in the array)
+			// of the photo we clicked on
+			// "this" is the element that was clicked on
+			// data("id") gets the attribute data-id
+			// (which we set to the index of the photo in
+			// the array - @index)
+			var index = $(this).data("id");
+
+			// set the current photo to this photo
+			current_project = portfolio.projects[index];
+			
+			// displays the single photo template
+			showTemplate(project_template, current_project);
+
+		});
+	});
+
+
+
+
+
 	// 
 	//  clicking on the slideshow tab displays the
 	//  current album as a slide show
@@ -148,9 +199,25 @@ $(document).ready(function(){
 		$("#slideshow-tab").addClass("active");
 	});
 
+
+	$("#home-tab").click(function () {
+		$(".nav-pills .active").removeClass("active");
+		$("#home-tab").addClass("active");
+		// $("#content").html($('#home_page_html').html());
+		var container = document.getElementById("content");
+		container.innerHTML = $('#home_page_html').html();
+	});
+
+
+	$("#contact-tab").click(function () {
+		$(".nav-pills .active").removeClass("active");
+		$("#contact-tab").addClass("active");
+		$("#content").html($('#contact_page_html').html());
+
+	});
 	// start the page by showing the albums view
 	// we do this by virtually clicking on the 
 	// albums tab
-	$("#albums-tab").click();
+	$("#home-tab").click();
 
 });
