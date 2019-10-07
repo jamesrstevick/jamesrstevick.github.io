e273cdeb188d6d76db9148fdea6d109f4c4f4bff
jamesstevick <jstevick@umich.edu>
2019-10-06 22:53:23 -0700

    Added new format for portfolio display

--- js/Projects.js
+++ js/Projects.js
@@ -12,52 +12,17 @@ var portfolio = {
             tags : ["hardware", "DIY", "course projects"],
         },
 		{
-            title : "Magnetic Levitator", 
-            pdfFile : "reports/MagLev.pdf",
-            thumbnail : "images/MagLev.jpg",
-            description : "The MagLev system maintains the position of a para-magnetic ball by using position sensors to control the driver circuit of an electromagnet",
+            title : "Image Segmentation", 
+            pdfFile : "reports/ImageSegmentation.pdf",
+            thumbnail : "images/ImageSegmentation.jpg",
+            description : 'Published paper in American Institute of Mathematical Sciences "Image Segmentation with Dynamic Artifacts Detection and Bias Correction" ',
             tags : ["hardware", "DIY", "course projects"]
         },
 		{
-            title : "Magnetic Levitator", 
-            pdfFile : "reports/MagLev.pdf",
-            thumbnail : "images/MagLev.jpg",
-            description : "The MagLev system maintains the position of a para-magnetic ball by using position sensors to control the driver circuit of an electromagnet",
-            tags : ["hardware", "DIY", "course projects"]
-        },
-		{
-            title : "Magnetic Levitator", 
-            pdfFile : "reports/MagLev.pdf",
-            thumbnail : "images/MagLev.jpg",
-            description : "The MagLev system maintains the position of a para-magnetic ball by using position sensors to control the driver circuit of an electromagnet",
-            tags : ["hardware", "DIY", "course projects"]
-        },
-		{
-            title : "Magnetic Levitator", 
-            pdfFile : "reports/MagLev.pdf",
-            thumbnail : "images/MagLev.jpg",
-            description : "The MagLev system maintains the position of a para-magnetic ball by using position sensors to control the driver circuit of an electromagnet",
-            tags : ["hardware", "DIY", "course projects"]
-        },
-		{
-            title : "Magnetic Levitator", 
-            pdfFile : "reports/MagLev.pdf",
-            thumbnail : "images/MagLev.jpg",
-            description : "The MagLev system maintains the position of a para-magnetic ball by using position sensors to control the driver circuit of an electromagnet",
-            tags : ["hardware", "DIY", "course projects"]
-        },
-		{
-            title : "Magnetic Levitator", 
-            pdfFile : "reports/MagLev.pdf",
-            thumbnail : "images/MagLev.jpg",
-            description : "The MagLev system maintains the position of a para-magnetic ball by using position sensors to control the driver circuit of an electromagnet",
-            tags : ["hardware", "DIY", "course projects"]
-        },
-		{
-            title : "Magnetic Levitator", 
-            pdfFile : "reports/MagLev.pdf",
-            thumbnail : "images/MagLev.jpg",
-            description : "The MagLev system maintains the position of a para-magnetic ball by using position sensors to control the driver circuit of an electromagnet",
+            title : "Network Theory", 
+            pdfFile : "reports/SeniorThesis.pdf",
+            thumbnail : "images/SeniorThesis.jpg",
+            description : 'Senior Thesis for B.A. in Physics "Developing Metrics for Networks Embedded in Physical Space"',
             tags : ["hardware", "DIY", "course projects"]
         }
 	]
